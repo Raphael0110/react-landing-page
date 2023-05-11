b62bcc958ee62e1d7959a4a0914b2451b91b3780
Ramml <Raphaelramml@gmail.com>
2023-05-11 09:41:09 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import styles from './app.Module.sccs';
+import React from 'react';
+import styles from './app.Module.scss';
 
 export default function App() {
   return (
-    <header>
-      <div className={styles.greenHeader}>kopfpart </div>
-    </header>
+    <>
+      <div className={styles.greenheader}>
+        Bolton Remote is now a part of SupportNinja!
+      </div>
+
+      <body>
+        <div>mittelteil</div>
+      </body>
+      <footer>endteil</footer>
+    </>
   );
 }
